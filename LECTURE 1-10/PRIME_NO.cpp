27152a2a371fae95d02f8141c813698ef1422b7a
Your Name <your-email@example.com>
2025-04-29 23:38:23 +0530

    lecture 29 and 30 commit

--- LECTURE 1-10/PRIME_NO.cpp
+++ LECTURE 1-10/PRIME_NO.cpp
@@ -1,20 +1,20 @@
-  #include<iostream>
- using namespace std;
- int main(){
-      int n;
-      cout<<"enter the no.";
-      cin>>n;
-      int i=2;
-      while(i<n){
-          if(n%i==0){
-              cout<<"number is not prime "<<i<<endl;
+//   #include<iostream>
+//  using namespace std;
+//  int main(){
+//       int n;
+//       cout<<"enter the no.";
+//       cin>>n;
+//       int i=2;
+//       while(i<n){
+//           if(n%i==0){
+//               cout<<"number is not prime "<<i<<endl;
               
-          }
-          cout<<"number is prime"<<endl;
-          i++;
-          }
+//           }
+//           cout<<"number is prime"<<endl;
+//           i++;
+//           }
          
-      }
+//       }
 
 // prime no using function
 
